370ed909d65a929704058be88ba1e766525b1bf6
Pablo Henrique <pablo_hen@hotmail.com>
2021-05-24 11:24:21 -0300

    montando dinamicamente + alts. para telas maiores

--- components/ChamadaNoticia.jsx
+++ components/ChamadaNoticia.jsx
@@ -1,27 +1,25 @@
 import React from 'react';
 
-const ChamadaNoticia = ({
-  titulo = 'Titulo',
-  subtitulo = 'Subtitulo',
-  texto = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer ac leo ac ante gravida venenatis. Praesent vel blandit quam.',
-  data = 'hoje',
-  categoria = 'Diversos',
-  imagem = false,
-}) => {
+const ChamadaNoticia = ({ noticia }) => {
+  const { titulo, subtitulo, corpo, data, categoria, imagem } = noticia;
+
   return (
-    <div className="bg-white border shadow-sm mt-4 p-6">
+    <div className="bg-white border shadow-sm mt-4 py-6">
       <div className="flex flex-col space-y-2">
-        <span className="font-bold">{subtitulo}</span>
-        <a href="#" className="text-primary text-2xl font-bold">
+        <span className="font-bold px-6">{subtitulo}</span>
+        <a
+          href="#"
+          className="text-primary text-2xl sm:text-4xl font-bold px-6 sm:pt-28"
+        >
           {titulo}
         </a>
-        <p className="text-gray-600">{texto}</p>
+        <p className="text-gray-600 px-6 sm:pb-28">{corpo}</p>
 
-        <div className="flex space-x-2 text-gray-600">
+        {imagem && <img src={imagem} alt={titulo} className="" />}
+        <div className="flex space-x-2 text-gray-600 px-6 text-xs sm:invisible">
           <span>{data}</span>
           <span>-</span>
           <span>Em {categoria}</span>
-          {imagem && <img src={imagem} alt={titulo} />}
         </div>
       </div>
     </div>

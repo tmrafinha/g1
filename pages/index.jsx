121f6670b41e196fcc3de5b73e1fa3f00f7ed755
Pablo Henrique <pablo_hen@hotmail.com>
2021-05-24 11:25:22 -0300

    alterações para carregar conteúdo dinamicamente

--- pages/index.jsx
+++ pages/index.jsx
@@ -4,16 +4,53 @@ import CardLateral from '../components/CardLateral';
 import ChamadaNoticia from '../components/ChamadaNoticia';
 import Rodape from '../components/Rodape';
 import BarraGloboCom from './../components/BarraGloboCom';
+import useDados from './../hooks/useDados';
 
 const Home = () => {
+  const { carregando, erro, dados } = useDados('http://localhost:3000/api');
+
+  if (carregando) {
+    return (
+      <div>
+        <p>Carregando...</p>
+      </div>
+    );
+  }
+
+  if (erro) {
+    return (
+      <div>
+        <p>Ocorreu um erro ao carregar. Tente em instantes</p>
+      </div>
+    );
+  }
+
+  const { noticias, menus } = dados;
+
   return (
     <div>
       <BarraGloboCom />
       <BarraTitulo />
-      <ChamadaNoticia />
-      <ChamadaNoticia />
-      <CardLateral />
-      <CardLateral cabecalho="Mais Lidas" />
+
+      <div className="flex flex-col lg:flex-row lg:space-x-4 lg:px-4">
+        <div className="w-full">
+          {noticias &&
+            noticias.map((noticia) => (
+              <ChamadaNoticia key={noticia.titulo} noticia={noticia} />
+            ))}
+        </div>
+
+        <div className="lg:w-96">
+          {menus &&
+            menus.map((menu) => (
+              <CardLateral
+                key={menu.nome}
+                cabecalho={menu.nome}
+                itens={menu.itens}
+              />
+            ))}
+        </div>
+      </div>
       <Rodape />
     </div>
   );

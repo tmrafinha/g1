84accbc78680752dd52c9daf2646255d02fb6d1d
Pablo Henrique <pablo_hen@hotmail.com>
2021-05-25 16:47:24 -0300

    mais mudanças em telas grandes

--- components/ChamadaNoticia.jsx
+++ components/ChamadaNoticia.jsx
@@ -12,11 +12,21 @@ const ChamadaNoticia = ({ noticia }) => {
         <div className="shadow-sm sm:shadow-none border-b py-6 sm:p-6 group transform transition-all hover:bg-white hover:border hover:shadow-md">
           <div className="flex flex-col sm:flex-row-reverse sm:justify-end space-y-2 sm:space-y-0">
             <div className="overflow-ellipsis overflow-hidden">
-              <span className="font-bold px-6">{subtitulo}</span>
-              <p className="text-primary group-hover:text-primaryHover text-2xl sm:text-4xl font-bold px-6 overflow-ellipsis overflow-hidden">
+              <span className={`font-bold px-6 ${!imagem ? ' sm:px-0' : ''}`}>
+                {subtitulo}
+              </span>
+              <p
+                className={`text-primary group-hover:text-primaryHover text-2xl sm:text-4xl font-bold px-6 ${
+                  !imagem ? ' sm:px-0' : ''
+                } overflow-ellipsis overflow-hidden`}
+              >
                 {titulo}
               </p>
-              <p className="text-gray-600 px-6 overflow-ellipsis overflow-hidden">
+              <p
+                className={`text-gray-600 px-6 ${
+                  !imagem ? ' sm:px-0' : ''
+                } overflow-ellipsis overflow-hidden`}
+              >
                 {chamada}
               </p>
             </div>

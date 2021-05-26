739879121b970f592a119960cdcfa90590026ba9
Pablo Henrique <pablo_hen@hotmail.com>
2021-05-26 15:30:16 -0300

    add efeito de transição no botão

--- components/LinkBarraGloboCom.jsx
+++ components/LinkBarraGloboCom.jsx
@@ -7,7 +7,13 @@ const LinkBarraGloboCom = ({
   botao = false,
 }) => {
   return (
-    <div className={`${botao ? 'bg-white border rounded px-2 shadow-sm' : ''}`}>
+    <div
+      className={`${
+        botao
+          ? 'bg-white border rounded px-2 shadow-sm transform transition-all hover:shadow-md'
+          : ''
+      }`}
+    >
       <a href={link} style={{ color: cor }}>
         {texto}
       </a>

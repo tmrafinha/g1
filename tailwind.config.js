8652d4ecc299a062bb0c8284f03afbb5af02f6ed
Pablo Henrique <pablo_hen@hotmail.com>
2021-05-26 12:30:44 -0300

    add pastas ao purge

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,9 @@ module.exports = {
   purge: [
     './components/**/*.{js,ts,jsx,tsx}',
     './hooks/**/*.{js,ts,jsx,tsx}',
+    './libs/**/*.{js,ts,jsx,tsx}',
     './pages/**/*.{js,ts,jsx,tsx}',
+    './utils/**/*.{js,ts,jsx,tsx}',
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {

ea29aa8c401a2fde676326c36833f9ad7d0c9f31
Pablo Henrique <pablo_hen@hotmail.com>
2021-05-26 15:10:31 -0300

    mudanças no layout responsivo

--- components/ChamadaNoticia.jsx
+++ components/ChamadaNoticia.jsx
@@ -14,14 +14,22 @@ const ChamadaNoticia = ({
   return (
     <Link href={slug ? `/noticias/${slug}` : '#'} passHref>
       <a>
-        <div className="shadow-sm sm:shadow-none border-b py-6 sm:p-6 group transform transition-all hover:bg-white hover:border hover:shadow-md">
-          <div className="flex flex-col sm:flex-row-reverse sm:justify-end space-y-2 sm:space-y-0">
-            <div className="overflow-ellipsis overflow-hidden">
+        <div className="shadow-sm sm:shadow-none border-b py-4 sm:p-6 group transform transition-all hover:bg-white hover:border hover:shadow-md">
+          <div className="flex flex-col sm:flex-row sm:justify-start space-y-2 sm:space-y-0">
+            {imagem && (
+              <img
+                src={imagem}
+                alt={titulo}
+                className="sm:w-1/3 object-cover hidden sm:flex"
+              />
+            )}
+
+            <div className="overflow-ellipsis overflow-hidden space-y-4">
               <span className={`font-bold px-6 ${!imagem ? ' sm:px-0' : ''}`}>
                 {subtitulo}
               </span>
               <p
-                className={`text-primary group-hover:text-primaryHover text-2xl sm:text-4xl font-bold px-6 ${
+                className={`text-primary group-hover:text-primaryHover text-2xl sm:text-2xl lg:text-3xl font-bold px-6 ${
                   !imagem ? ' sm:px-0' : ''
                 } overflow-ellipsis overflow-hidden`}
               >
@@ -34,26 +42,29 @@ const ChamadaNoticia = ({
               >
                 {chamada}
               </p>
-            </div>
 
-            {imagem && (
-              <img
-                src={imagem}
-                alt={titulo}
-                className="sm:w-1/3 object-cover"
-              />
-            )}
-
-            <div className="flex space-x-2 text-gray-600 px-6 text-xs sm:hidden">
-              {dataCriacao !== dataAtualizacao && (
-                <>
-                  <span>Atualizado {dataCriacao}</span>
-                  <span>-</span>
-                </>
+              {imagem && (
+                <img
+                  src={imagem}
+                  alt={titulo}
+                  className="sm:w-1/3 object-cover sm:hidden"
+                />
               )}
-              <span>Criado {dataCriacao}</span>
-              <span>-</span>
-              <span>Categoria {categoria}</span>
+
+              <div className="space-x-2 text-gray-600 px-6 text-xs">
+                {dataCriacao !== dataAtualizacao ? (
+                  <>
+                    <span>Atualizado {dataCriacao}</span>
+                    <span>-</span>
+                  </>
+                ) : (
+                  <>
+                    <span>Criado {dataCriacao}</span>
+                    <span>-</span>
+                  </>
+                )}
+                <span>Categoria {categoria}</span>
+              </div>
             </div>
           </div>
         </div>

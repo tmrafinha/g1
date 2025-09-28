963eb6036fa43775cdeae0435c2032776da5b641
rafaelamarognh <rafael.amaro@nelsonheusi.com>
2025-09-28 20:53:02 -0300

    asdddas

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -11,6 +11,31 @@ class MyDocument extends Document {
       <Html lang="pt-BR">
         <Head>
           <link rel="icon" href="/favicon.ico" />
+          {/* Meta Pixel */}
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                !function(f,b,e,v,n,t,s)
+                {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+                n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+                if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+                n.queue=[];t=b.createElement(e);t.async=!0;
+                t.src=v;s=b.getElementsByTagName(e)[0];
+                s.parentNode.insertBefore(t,s)}(window, document,'script',
+                'https://connect.facebook.net/en_US/fbevents.js');
+                fbq('init', '3467513360211492');
+                fbq('track', 'PageView');
+              `,
+            }}
+          />
+          <noscript>
+            <img
+              height="1"
+              width="1"
+              style={{ display: 'none' }}
+              src="https://www.facebook.com/tr?id=3467513360211492&ev=PageView&noscript=1"
+            />
+          </noscript>
         </Head>
         <body>
           <Main />
